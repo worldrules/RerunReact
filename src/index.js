9b27bd800cdc15ad0215b010d015b22ad6dfa0db
worldrules <worldrulesdev@gmail.com>
2021-05-13 01:33:42 -0300

    add faker lib

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi There !</div>;
-};
+const App = () => (
+    <div className="ui container comments">
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar"></img>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                      Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">
+                    Nice Blog post !
+                </div>
+            </div>
+        </div>
+    </div>
+);
 
 ReactDOM.render(<App/>,document.querySelector('#root')); 
  
