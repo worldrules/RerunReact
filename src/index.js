12a15071a49a7dab547275a4f3deb2bcb77c4b89
worldrules <worldrulesdev@gmail.com>
2021-05-13 02:05:29 -0300

    comment component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  faker  from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => (
     <div className="ui container comments">
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}></img>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                      Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
-                </div>
-                <div className="text">
-                    Nice Blog post !
-                </div>
-            </div>
-        </div>
+     <CommentDetail author="Sam"></CommentDetail>
+     <CommentDetail author="Alex"></CommentDetail>
+     <CommentDetail author="Jane"></CommentDetail>
     </div>
 );
 
